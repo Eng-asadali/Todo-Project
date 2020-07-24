ed60503ecf18020aa576259145e418194b6b33e6
Asad Ali <you@example.com>
2020-07-24 17:00:07 +0500

    update

--- todo/todoApp/views.py
+++ todo/todoApp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TodoForm
 from .models import Todo
 
 def todo_list(request):
@@ -17,3 +18,25 @@ def todo_detail(request,id):
         "todo": todo
     }
     return render(request, 'todo_detail.html', context)
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+       form.save()
+       return redirect('/')
+    context = {"form": form}
+    return render(request, "todo_create.html", context)
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {"form" : form}
+    return render(request, "todo_update.html", context)
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect("/")
