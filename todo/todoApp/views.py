24676ff9754d4b2ceb815e1691a949c957bbad0f
Asad Ali <you@example.com>
2020-07-24 15:48:50 +0500

    update

---
+++ todo/todoApp/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Todo
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    print(todos)
+    context = {
+        "todo_list" : todos
+    }
+    return render(request, "todo_list.html", context)
+# Create
+
+def todo_detail(request,id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        "todo": todo
+    }
+    return render(request, 'todo_detail.html', context)
